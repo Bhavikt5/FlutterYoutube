ef52ed6ff8a3322f639dc238c6be7c5b3cd16314
Bhavikt5 <bhaviktank5@gmail.com>
2022-06-28 21:29:52 +0530

    day 4

--- myapp/lib/login_page.dart
+++ myapp/lib/login_page.dart
@@ -1,15 +1,59 @@
+// ignore_for_file: unnecessary_const, prefer_const_constructors, sort_child_properties_last, avoid_print
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20.0,
+            width: 2.0,
+          ),
+          Image.asset(
+            "assets/images/login_img.png",
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Welcome",
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Enter Username", labelText: "Username"),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Enter Password", labelText: "Password"),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print("hi Bhavik");
+                },
+                child: Text("Login"),
+                style: TextButton.styleFrom(),
+              )
+            ]),
+          )
+        ],
       ),
     );
   }
--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/home_page.dart';
 import 'package:myapp/login_page.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.dark,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
-        "/": (context) => HomePage(),
-        "/Login": (context) => LoginPage()
+        "/": (context) => const LoginPage(),
       },
     );
   }
